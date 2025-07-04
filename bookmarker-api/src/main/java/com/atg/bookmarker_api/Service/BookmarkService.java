3beb6ffaad418abde664ae624a4f297ddd0ff52b
Mohammed-Akramuddin <mohdakram6174@gmail.com>
2025-07-04 23:03:50 +0530

    Update BookmarkService.java

--- bookmarker-api/src/main/java/com/atg/bookmarker_api/Service/BookmarkService.java
+++ bookmarker-api/src/main/java/com/atg/bookmarker_api/Service/BookmarkService.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package com.atg.bookmarker_api.Service;
 
 import com.atg.bookmarker_api.BookmarkMapper;
@@ -22,28 +22,5 @@ public class BookmarkService {
         return new BookmarksDTO(bookmarkRepository.findAllBookmarkDTO(pageable));
     }
 }
-=======
-package com.atg.bookmarker_api.Service;
 
-import com.atg.bookmarker_api.BookmarkMapper;
-import com.atg.bookmarker_api.BookmarksDTO;
-import com.atg.bookmarker_api.Repository.BookmarkRepository;
-import jakarta.transaction.Transactional;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.stereotype.Service;
 
-@Service
-@Transactional
-public class BookmarkService {
-    @Autowired
-    private BookmarkRepository bookmarkRepository;
-    public BookmarksDTO getBookmark(Integer page){
-        int pg=page<1?0:page-1;
-        Pageable pageable= PageRequest.of(pg,10, Sort.Direction.DESC,"createdAt");
-        return new BookmarksDTO(bookmarkRepository.findAllBookmarkDTO(pageable));
-    }
-}
->>>>>>> d79f437826b6ba6d025115917b21d6efd7fd153f
