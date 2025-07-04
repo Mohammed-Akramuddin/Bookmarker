2eff978484b943c1bbe47c0a6adace66b89fb380
Mohammed-Akramuddin <mohdakram6174@gmail.com>
2025-07-04 23:01:44 +0530

    Update BookmarkMapper.java

--- bookmarker-api/src/main/java/com/atg/bookmarker_api/BookmarkMapper.java
+++ bookmarker-api/src/main/java/com/atg/bookmarker_api/BookmarkMapper.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package com.atg.bookmarker_api;
 
 import com.atg.bookmarker_api.model.Bookmark;
@@ -15,21 +15,3 @@ public class BookmarkMapper {
         return bookmarkDTO;
     }
 }
-=======
-package com.atg.bookmarker_api;
-
-import com.atg.bookmarker_api.model.Bookmark;
-import org.springframework.stereotype.Component;
-
-@Component
-public class BookmarkMapper {
-    public BookmarkDTO toBookmarkDTO(Bookmark bookmark) {
-        BookmarkDTO bookmarkDTO = new BookmarkDTO();
-        bookmarkDTO.setId(bookmark.getId());
-        bookmarkDTO.setTitle(bookmark.getTitle());
-        bookmarkDTO.setUrl(bookmark.getUrl());
-        bookmarkDTO.setCreatedAt(bookmark.getCreatedAt());
-        return bookmarkDTO;
-    }
-}
->>>>>>> d79f437826b6ba6d025115917b21d6efd7fd153f
