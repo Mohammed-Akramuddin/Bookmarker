2c6ecb39e289b4285e8eab78bae1d731ae59684a
Mohammed-Akramuddin <mohdakram6174@gmail.com>
2025-07-05 15:13:24 +0530

    Add files via upload

---
+++ bookmarker-api/src/main/java/com/atg/bookmarker_api/model/BookmarkRequest.java
@@ -0,0 +1,14 @@
+package com.atg.bookmarker_api.model;
+
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Data;
+import org.antlr.v4.runtime.misc.NotNull;
+
+@Data
+public class BookmarkRequest {
+    @NotEmpty(message = "Titls should not be empty")
+    private String title;
+    @NotEmpty(message = "Url should not be empty")
+    private String Url;
+}
