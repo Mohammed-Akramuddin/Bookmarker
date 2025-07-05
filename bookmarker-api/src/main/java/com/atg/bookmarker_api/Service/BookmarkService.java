df4004caebc4c7a592fd11d22859e0d4a9bee0bc
Mohammed-Akramuddin <mohdakram6174@gmail.com>
2025-07-05 15:11:49 +0530

    Update BookmarkService.java

--- bookmarker-api/src/main/java/com/atg/bookmarker_api/Service/BookmarkService.java
+++ bookmarker-api/src/main/java/com/atg/bookmarker_api/Service/BookmarkService.java
@@ -1,21 +1,29 @@
-
 package com.atg.bookmarker_api.Service;
 
+import com.atg.bookmarker_api.BookmarkDTO;
 import com.atg.bookmarker_api.BookmarkMapper;
 import com.atg.bookmarker_api.BookmarksDTO;
 import com.atg.bookmarker_api.Repository.BookmarkRepository;
+import com.atg.bookmarker_api.model.Bookmark;
+import com.atg.bookmarker_api.model.BookmarkRequest;
 import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
+
 @Service
 @Transactional
 public class BookmarkService {
     @Autowired
     private BookmarkRepository bookmarkRepository;
+    @Autowired
+    private BookmarkMapper bookmarkMapper;
+    @Transactional
     public BookmarksDTO getBookmark(Integer page){
         int pg=page<1?0:page-1;
         Pageable pageable= PageRequest.of(pg,10, Sort.Direction.DESC,"createdAt");
@@ -25,8 +33,13 @@ public class BookmarkService {
     public BookmarksDTO searchBookmark(String query, Integer page) {
         int pg=page<1?0:page-1;
         Pageable pageable= PageRequest.of(pg,10, Sort.Direction.DESC,"createdAt");
-        return new BookmarksDTO(bookmarkRepository.findByTitleContainsIgnoreCase(query,pageable));
+        return new BookmarksDTO(bookmarkRepository.searchBookmark(query,pageable));
+    }
+    @Transactional
+    public BookmarkDTO createBookmark(@Valid BookmarkRequest bookmarkRequest) {
+        Bookmark b=new Bookmark(null,bookmarkRequest.getTitle(),bookmarkRequest.getUrl(),Instant.now());
+        Bookmark c=bookmarkRepository.save(b);
+        return bookmarkMapper.toBookmarkDTO(c);
+
     }
 }
-
-
